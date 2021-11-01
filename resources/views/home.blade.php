e7f6f4dac69fbbfb20d9e34799f7db7a84236b6d
adeerwin <gitndcjakarta@gmail.com>
2021-11-01 10:29:31 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,9 +51,41 @@
       </div>
       <div class="col-sm-8">
         <div class="raw">
-          <div class="car">
+          <div class="card">
             <div class="card-header bg-info">
               <span>Power IT Equipment</span>
+            </div>
+            <div class="card-body">
+              <div id="container" style="width: 740px; height: 150px; margin: 0 auto"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="raw">
+          <div class="card">
+            <div class="card-body">
+              PUE
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="raw">
+          <div class="card">
+            <div class="card-body">
+              Power Capacity
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="raw">
+          <div class="card">
+            <div class="card-body">
+              Rack Capacity
             </div>
           </div>
         </div>
@@ -62,4 +94,64 @@
   </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
+@endsection
+
+@section('data')
+<script language="JavaScript">
+  $(function() {
+    $("#example1").DataTable({
+      "responsive": true,
+      "lengthChange": true,
+      "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+  });
+
+  $(document).ready(function() {
+    var title = {
+      text: ''
+    };
+    var subtitle = {
+      text: ''
+    };
+    var xAxis = {
+      categories: [1, 2, 3, 4, 5, 6, 7, 8]
+    };
+    var yAxis = {
+      title: {
+        text: ''
+      },
+      plotLines: [{
+        value: 0,
+        width: .1,
+        color: '#808080'
+      }]
+    };
+    var tooltip = {
+      valueSuffix: ' Kw'
+    }
+    var legend = {
+      layout: 'horizontal',
+      align: 'right',
+      verticalAlign: 'bottom',
+      borderWidth: 0
+    };
+    var series = [{
+      name: '',
+      data: [10.32, 10.22, 10.43, 10.14, 10.25, 10.46, 10.27, 10.30]
+    }]
+
+    var json = {};
+    json.title = title;
+    json.subtitle = subtitle;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    json.tooltip = tooltip;
+    json.legend = legend;
+    json.series = series;
+
+    $('#container').highcharts(json);
+  });
+</script>
 @endsection
